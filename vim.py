788afb93b9d56e3ae0d8f2179bd3a949e39565e6
vim <vimdelarosa@gmail.com>
2020-12-08 19:28:02 +0800

    modified turtle

--- vim.py
+++ vim.py
@@ -3,13 +3,20 @@ import turtle
 vim_turtle = turtle.Turtle()
 vim_turtle.speed(1)
 
-vim_turtle.forward(100)
-vim_turtle.right(90)
-vim_turtle.forward(100)
-vim_turtle.right(90)
-vim_turtle.forward(100)
-vim_turtle.right(90)
-vim_turtle.forward(100)
+# Square
+
+def square():
+    vim_turtle.forward(100)
+    vim_turtle.right(90)
+    vim_turtle.forward(100)
+    vim_turtle.right(90)
+    vim_turtle.forward(100)
+    vim_turtle.right(90)
+    vim_turtle.forward(100)
+
+print(square())
+
+
 
 
 
