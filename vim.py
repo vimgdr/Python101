cd157faf247f3ddbacfabe494aee5b42d2c43c9f
vim <vimdelarosa@gmail.com>
2020-12-08 19:20:30 +0800

    turtle

--- vim.py
+++ vim.py
@@ -1,1 +1,15 @@
-import os
+import turtle
+
+vim_turtle = turtle.Turtle()
+vim_turtle.speed(1)
+
+vim_turtle.forward(100)
+vim_turtle.right(90)
+vim_turtle.forward(100)
+vim_turtle.right(90)
+vim_turtle.forward(100)
+vim_turtle.right(90)
+vim_turtle.forward(100)
+
+
+
